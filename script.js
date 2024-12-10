63ca8c9b353da5913722f61f5cad8bd7badf56ec
NotRhea <143529100+NotRhea@users.noreply.github.com>
2024-12-10 16:02:03 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function () {
-    const hero = document.querySelector(".hero");
-    let offset = window.pageYOffset;
-    hero.style.backgroundSize = 100 + offset * 0.1 + "%";
-});
